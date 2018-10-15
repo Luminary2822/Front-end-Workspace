b84b3b8504e208f98ac8d8cfadbc02d749a856ac
Luminary <809947110@qq.com>
2018-10-15 20:21:21 +0800

    20181015

---
+++ my-project/src/components/NewTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="NewTest">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NewTest',
+    data () {
+      return {
+        msg: '这是一个新添加的组件'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Test',
+    data () {
+      return {
+        msg: '这是一个默认渲染的测试组件'
+      }
+    }
+  }
+</script>
